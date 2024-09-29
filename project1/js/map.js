c11a559b6e3e99432991d475219aa86764e4eebd
olijwood <olijwood@gmail.com>
2024-09-29 11:56:48 +0100

    fix stadia maps auth issue

--- project1/js/map.js
+++ project1/js/map.js
@@ -290,7 +290,7 @@ const labels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/
   maxZoom: 19,
   attribution: '&copy; CartoDB'
 });
-const terrainLines = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png', {
+const terrainLines = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png?api_key=408797ca-05bb-4a64-9de8-7a9248c241ae', {
   maxZoom: 20,
   attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>', 
 });
