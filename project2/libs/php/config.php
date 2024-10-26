00db553f67a7e3a9b6378928b85eaeb10853bade
olijwood <olijwood@gmail.com>
2024-10-26 16:06:03 +0100

    Change to using env variables for db

--- project2/libs/php/config.php
+++ project2/libs/php/config.php
@@ -1,14 +1,19 @@
 
 <?php
-	// connection details for MySQL database
+
+	use Dotenv\Dotenv;  
+
+	require '../../../assets/vendor/autoload.php';
+
+	$dotenv = Dotenv::createImmutable(__DIR__ . '/../../'); // Adjust this path to match your folder structure
+	$dotenv->load();
+
+	$cd_dbname = $_ENV['DBNAME'];
+	$cd_user = $_ENV['DBUSER'];
+	$cd_password = $_ENV['DBPW'];
 
 	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
 
-	// database name, username and password
-
-	$cd_dbname = "companydirectory";
-	$cd_user = "companydirectory";
-	$cd_password = "companydirectory";
 ?>
